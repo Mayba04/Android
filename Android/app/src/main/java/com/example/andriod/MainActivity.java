c9ecd065e1788bb788afd39ed19085e6a3f2c1a5
Pasha Mayba <pashamayba7@gmail.com>
2024-04-14 16:13:54 +0300

    Initial commit

---
+++ Android/app/src/main/java/com/example/andriod/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.andriod;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ImageView ivAvatar = findViewById(R.id.imageView);
+        String url = "https://i.pinimg.com/564x/a8/4b/94/a84b94d816597cea9c83cb64d9d7e0e7.jpg";
+        Glide.with(this)
+                .load(url)
+                .apply(new RequestOptions().override(400))
+                .into(ivAvatar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
