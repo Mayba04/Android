19d4d318803eaa6a2581856a1dc6adf8444bd561
Pasha Mayba <pashamayba7@gmail.com>
2024-04-16 14:58:41 +0300

    Add Core lib

---
+++ WebApi_pulse/Core/Interfaces/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryDTO>> GetAllAsync();
+        Task<CategoryDTO?> GetByIdAsync(int id);
+        Task CreateAsync(CreateCategoryDTO createCategoryDTO);
+        Task DeleteCategoryByIDAsync(int id);
+        Task EditAsync(EditCategoryDTO editCategoryDTO);
+    }
+}
---
+++ WebApi_pulse/CreateCategoryDTO.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class CreateCategoryDTO
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
+}
